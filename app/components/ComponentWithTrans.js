680dd558c882a880167e93eab0121a96275165e8
Lone <salman.lone@monster.com>
2020-02-04 17:36:29 +0800

    refactoring

--- app/components/ComponentWithTrans.js
+++ app/components/ComponentWithTrans.js
@@ -1,12 +1,10 @@
-import React from 'react'
-import { Trans } from 'react-i18next'
+import React from "react";
+import { Trans } from "react-i18next";
 
-export default function ComponentWithTrans () {
+export default function ComponentWithTrans() {
   return (
     <p>
-      <Trans i18nKey='common:transComponent'>
-        Alternatively, you can use <code>Trans</code> component.
-      </Trans>
+      <Trans i18nKey="common:transComponent">Translate</Trans>
     </p>
-  )
+  );
 }
