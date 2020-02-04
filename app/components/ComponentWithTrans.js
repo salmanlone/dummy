f9f3a236e6b93a2254a382649afa5c7616ce976e
Lone <salman.lone@monster.com>
2020-02-04 17:58:19 +0800

    translation working

--- app/components/ComponentWithTrans.js
+++ app/components/ComponentWithTrans.js
@@ -5,6 +5,7 @@ export default function ComponentWithTrans() {
   return (
     <p>
       <Trans i18nKey="common:transComponent">Translate</Trans>
+      <Trans i18nKey="common:testing">Hi</Trans>
     </p>
   );
 }
