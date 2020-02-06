ef28610a20cb9c931af1bce6bb906b4c38d5b237
Lone <salman.lone@monster.com>
2020-02-06 18:20:50 +0800

    refactoring

--- pages/demoRedux.js
+++ pages/demoRedux.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { compose } from "redux";
-import { increment } from "../app/actions";
+import { increment, loadData } from "../app/actions";
 import { withI18next } from "../app/lib/withI18next";
+
 export class componentName extends Component {
   Increament = () => {
     this.props.Increament();
   };
+
   render() {
     console.log("test", this.props);
     const { t } = this.props;
     return (
       <div>
         <p>{this.props.count}</p>
-        <button onClick={this.Increament}>{t("bnt_text")}</button>
+        <button onClick={this.props.load}>{t("bnt_text")}</button>
       </div>
     );
   }
@@ -27,6 +29,10 @@ const mapDispatchToProps = dispatch => {
   return {
     Increament: () => {
       dispatch(increment());
+    },
+    load: () => {
+      console.log("load");
+      dispatch(loadData());
     }
   };
 };
