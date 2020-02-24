8775133ff4d355a3ae9366c2a8a713587c3b4f21
Hoong <shawn.hoong@monster.com>
2020-02-24 16:06:14 +0800

    Server side language changing, requires IIS setup with custom server variable

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,50 @@ import { appWithTranslation, i18n } from "../i18n";
 import { compose } from "redux";
 
 class MyApp extends App {
+  static async getInitialProps(props) {
+    const { ctx } = props;
+    const { req, res } = ctx;
+
+    let theLanguage = ""
+    let domainList = [
+      ["www.monster.com", "en"],
+      ["www.monster.de", "de"],
+      ["www.monster.fr", "fr"],
+      ["www.monster.ca", "en"]
+    ]
+
+    if (req) {
+
+      const foobar = req.headers["foobar-url"]
+      console.log("myheaders", req.headers)
+
+
+      for (let i = 0; i < domainList.length; i++) {
+        if (foobar.includes(domainList[i][0])) {
+          theLanguage = domainList[i][1]
+          // console.log("AAAAAAA", domainList[i][1])
+        }
+      }
+      // console.log("aFOOBAR", foobar)
+
+      // console.log("aFOOBAR", req.headers["foobar-url"])
+      // http://www.monster.de:80/de
+      // 19|de|
+      // get from foo bar to set language, pass to props
+    }
+
+    const appProps = await App.getInitialProps(props);
+
+    // set props here, access from apge
+    i18n.changeLanguage(theLanguage)
+    appProps.pageProps = {
+      ...appProps.pageProps,
+      theLanguage
+    };
+
+    return { ...appProps };
+  }
+
   render() {
     const { Component, pageProps, store, router } = this.props;
 
@@ -21,34 +65,9 @@ class MyApp extends App {
   }
 }
 
-// MyApp.getInitialProps = async () => (
-//   let pageProps = {}
-//   {namespacesRequired: ["common"]}
-//   );
-
-// MyApp.getInitialProps = async () => {
-//   let pageProps = {};
-//   let namespacesRequired = ["common"];
-//   return { pageProps, namespacesRequired };
-// };
-
-// export default compose(
-//   withRedux(createStore),
-//   withReduxSaga({ async: true })
-// )(appWithTranslation(MyApp));
 
 export default compose(
   withRedux(createStore),
   withReduxSaga({ async: true })
 )(appWithTranslation(MyApp));
 
-// export default appWithTranslation(
-//   compose(withRedux(createStore), withReduxSaga({ async: true }))(MyApp)
-// );
-
-// export default appWithTranslation(
-//   withRedux(createStore)(withReduxSaga({ async: true }))(new MyApp())
-//   // MyApp
-// );
-
-// export default compose(connect(mapStateToProps, mapDispatchToProps))(Test);
