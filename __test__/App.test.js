591a1a6eb663af9bd9fc822df015e24669fbcff6
Lone <salman.lone@monster.com>
2020-02-24 10:54:33 +0800

    removed unwanted test

--- __test__/App.test.js
+++ __test__/App.test.js
@@ -2,16 +2,7 @@ import { shallow } from "enzyme";
 import React from "react";
 import renderer from "react-test-renderer";
 
-import App from "../pages/App.js";
 import Autocomplete from "../app/components/Autocomplete";
-
-describe("With Enzyme", () => {
-  it('App shows "Hello, Sunshine!"', () => {
-    const app = shallow(<App />);
-
-    expect(app.find("div").text()).toEqual("Hello, Sunshine!");
-  });
-});
 
 describe("Autocomplete component", () => {
   it("verify the placeholder text", () => {
@@ -21,13 +12,5 @@ describe("Autocomplete component", () => {
   it("verify the type", () => {
     const autocomplete = shallow(<Autocomplete />);
     expect(autocomplete.find("input").type()).toEqual("input");
-  });
-});
-
-describe("With Snapshot Testing", () => {
-  it('App shows "Hello, Sunshine!"', () => {
-    const component = renderer.create(<App />);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
   });
 });
