0915f36dc38adc5974d7d53743d90ac4d8d05e82
Lone <salman.lone@monster.com>
2020-03-02 14:49:20 +0800

    unit tests

---
+++ __test__/salary/reducer.test.js
@@ -0,0 +1,40 @@
+import produce from "immer";
+
+import homeReducer from "../../app/reducer/homeReducer";
+import { loadPositionListingSuccess } from "../../app/actions/salaryDetail";
+
+describe("homeReducer", () => {
+  let state;
+
+  beforeEach(() => {
+    state = {
+      position: "",
+      location: ""
+    };
+  });
+
+  it("should return the initial state", () => {
+    const expectedResult = state;
+    expect(homeReducer(undefined, {})).toEqual(expectedResult);
+  });
+
+  it("should handle the positions listing response action correctly", () => {
+    const positionsListing = [
+      "Software Developer / Engineer",
+      "Software Architect",
+      "Embedded Software Engineer",
+      "Senior Software Developer / Engineer",
+      "Software Development / Engineering Manager",
+      "Senior Java Developer / Engineer",
+      "JavaScript Developer / Engineer",
+      "Android Developer / Engineer"
+    ];
+    const expectedResult = produce(state, draft => {
+      draft.positions = positionsListing;
+    });
+
+    expect(
+      homeReducer(state, loadPositionListingSuccess(positionsListing))
+    ).toEqual(expectedResult);
+  });
+});
