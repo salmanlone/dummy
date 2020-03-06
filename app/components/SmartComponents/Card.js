a44b0c6d6e0beb442897aa77ae230a018319b0ba
Hoong <shawn.hoong@monster.com>
2020-03-06 17:46:39 +0800

    SalaryResults replication from production commit

--- app/components/SmartComponents/Card.js
+++ app/components/SmartComponents/Card.js
@@ -4,11 +4,31 @@ const Card = props => {
   return (
     <div style={props.style} key={props.index}>
       <h3>{props.title}</h3>
-      <hr />
+      {props.horizontalRule ?
+        <hr />
+        : ""}
       <p>{props.blurb}</p>
-      {props.accessNameProp
-        ? props.data.map(job => <div key={job.index}>{job.name}</div>)
-        : props.data.map(job => <div key={job.index}>{job}</div>)}
+      {props.cardType == "boost" || props.cardType == "advance"
+        ? props.data.map(job =>
+          <div key={job.index}>{job.name}</div>
+        ) : ""
+      }
+      {props.cardType == "salary"
+        ? props.data.map(job =>
+          <div key={job.index}>{job}</div>
+        ) : ""
+      }
+      {props.cardType == "related"
+        ?
+        <div>
+          <div>{props.data.PostedDateMessage}</div>
+          <div key={props.data.JobId}>{props.data.Title}</div>
+          <div>{props.data.Company}</div>
+          <div>{props.data.PhysicalCityName}, {props.data.PhysicalStateAbbr}</div>
+        </div>
+        : ""
+      }
+
     </div>
   );
 };
--- pages/SalaryResult.js
+++ pages/SalaryResult.js
@@ -6,17 +6,46 @@ import { withTranslation, Link, i18n } from "../i18n";
 import { Trans } from "react-i18next";
 import { createStructuredSelector } from "reselect";
 
-import salaryDummyData from "../app/SalaryDetailDummy";
+import salaryDummyData from "../app/DummyData/SalaryDetailDummy";
+import relatedJobsDummyData from '../app/DummyData/RelatedJobsDummy';
 
 import BreadCrumb from "../app/components/DumbComponents/Breadcrumb";
 
 import Card from "../app/components/SmartComponents/Card";
 import SalaryGraph from "../app/components/SmartComponents/SalaryGraph";
+import RelatedJobs from "../app/components/SmartComponents/relatedJobs";
 import { makeSelectSalaryResultResponse } from "../app/containers/SalaryResult/selector";
 
+const location = "Boston, MA"
+
 const myStyle = {
-  border: "1px solid"
+  border: "1px solid",
+  'box-sizing': 'border-box'
 };
+
+const miniBox = {
+  border: '1px solid',
+  width: '50%'
+}
+const column = {
+  float: 'left',
+  width: '33.33%',
+  padding: '10px',
+  height: '250px'
+}
+
+const column100 = {
+  width: '812px',
+  float: 'left',
+  padding: '10px'
+}
+
+
+const row = {
+  content: "",
+  display: "table",
+  clear: "both"
+}
 
 const SalaryResult = ({ t, SalaryResultResponse }) => (
   <div>
@@ -24,29 +53,44 @@ const SalaryResult = ({ t, SalaryResultResponse }) => (
       <p>{SalaryResultResponse}</p>
       <p> {t("testing")}</p>
       <BreadCrumb />
-      <SalaryGraph style={myStyle} salary={salaryDummyData} />
-      <div>
+      <SalaryGraph
+        style={{ ...myStyle, ...column100 }}
+        location={location}
+        salary={salaryDummyData}
+      />
+      <div style={row}>
         <Card
-          style={myStyle}
+          style={{ ...myStyle, ...column }}
+          horizontalRule={true}
           title={"Boost Your Value"}
           blurb={"These skills can up your value"}
-          accessNameProp={true}
+          cardType={'boost'}
           data={salaryDummyData.byLocation.distinguishingSkills}
         />
         <Card
-          style={myStyle}
+          style={{ ...myStyle, ...column }}
+          horizontalRule={true}
           title={"Salary for Related Jobs"}
-          accessNameProp={false}
+          cardType={'salary'}
           data={salaryDummyData.similarOccupation.slice(0, 5)}
         />
         <Card
-          style={myStyle}
+          style={{ ...myStyle, ...column }}
+          horizontalRule={true}
           title={"Advanced your career"}
           blurb={"These are potential next steps in this career path"}
-          accessNameProp={true}
+          cardType={'advance'}
           data={salaryDummyData.advancementOccupations
             .filter(job => job.category == "AdvancementJob")
             .slice(0, 3)}
+        />
+      </div>
+      <div style={row}>
+        <RelatedJobs
+          style={{ ...myStyle, ...column100 }}
+          boxStyle={miniBox}
+          title={salaryDummyData.name + ' Jobs in ' + location + ' on Monster'}
+          data={relatedJobsDummyData}
         />
       </div>
     </Layout>
