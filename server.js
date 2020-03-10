bd96c25ec1f134725f0ddd4c0882bbe6cb68d429
Lone <salman.lone@monster.com>
2020-03-10 11:58:38 +0800

    unwanted changes checked in

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const getLangFromDomain = request => {
       req.i18n.changeLanguage(language);
     }
 
-    const actualPage = "/home";
+    const actualPage = "/salary";
     app.render(req, res, actualPage);
   });
 
