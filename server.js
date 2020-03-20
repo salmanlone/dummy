8f35eec0ab2fa54f2b54dbe1c4570b3d59ed06c5
Lone <salman.lone@monster.com>
2020-03-20 13:34:29 +0800

    SSR routes for salary path

--- server.js
+++ server.js
@@ -9,24 +9,6 @@ const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const getLangFromDomain = request => {
-  let domainList = [
-    ["www.monster.com", "en"],
-    ["www.monster.ca", "en"],
-    ["www.monster.de", "de"],
-    ["www.monster.fr", "fr"]
-  ];
-  if (request.headers["foobar-url"]) {
-    const foobar = request.headers["foobar-url"];
-    for (let i = 0; i < domainList.length; i++) {
-      if (foobar.includes(domainList[i][0])) {
-        return domainList[i][1];
-      }
-    }
-  }
-  return "";
-};
-
 (async () => {
   await app.prepare();
   const server = express();
@@ -34,51 +16,29 @@ const getLangFromDomain = request => {
   await nextI18next.initPromise;
   server.use(nextI18NextMiddleware(nextI18next));
 
-  //  Custom Routes for the salary application
-
-  // '?p-position&l-location'
-  server.get("/", (req, res) => {
-    let language = getLangFromDomain(req);
-    if (language != "") {
-      req.i18n.changeLanguage(language);
-    }
-
-    const actualPage = "/salary";
-    app.render(req, res, actualPage);
-  });
-
   server.get("/salary", (req, res) => {
-    let language = getLangFromDomain(req);
-    if (language != "") {
-      req.i18n.changeLanguage(language);
-    }
-
+    req.i18n.changeLanguage("en");
     const actualPage = "/salary";
     app.render(req, res, actualPage);
   });
 
-  server.get("/salary/:position/:location", (req, res) => {
-    const actualPage =
-      "/salary/" + req.params.position + "-" + req.params.location;
-    const queryParams = {
-      position: req.params.position,
-      location: req.params.location
-    };
-
-    //  some data we might need to use to do the checking of the routes to render the app at first time
-    console.log("original Url :: ", req.headers.host);
-    console.log("original Url :: ", req.originalUrl);
-    console.log("base url :: ", req.baseUrl);
-    console.log("query :: ", req.query);
-    console.log("params :: ", req.params);
-    console.log("query params :: ", req.queryParams);
-
-    console.log("path :: ", req._parsedUrl.path);
-    console.log("path name :: ", req._parsedUrl.pathname);
-    console.log("href :: ", req._parsedUrl.href);
-
-    app.render(req, res, actualPage, queryParams);
+  server.get("/salaire", (req, res) => {
+    req.i18n.changeLanguage("fr");
+    const actualPage = "/salary";
+    app.render(req, res, actualPage);
   });
+
+  server.get("/gehalt", (req, res) => {
+    req.i18n.changeLanguage("de");
+    const actualPage = "/salary";
+    app.render(req, res, actualPage);
+  });
+
+  // server.get("/salaire/:query", (req, res) => {
+  //   req.i18n.changeLanguage("fr");
+  //   const actualPage = "/salary/query";
+  //   app.render(req, res, actualPage);
+  // });
 
   server.get("*", (req, res) => {
     handle(req, res);
