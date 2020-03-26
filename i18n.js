f8c9dbf156246d5542dc65ba8d9c3def4caf3bb9
Lone <salman.lone@monster.com>
2020-03-26 15:06:30 +0800

    removed commented code

--- i18n.js
+++ i18n.js
@@ -1,32 +1,12 @@
 const NextI18Next = require("next-i18next").default;
-// const { localeSubpaths } = require("next/config").default().publicRuntimeConfig;
-
-// const options = {
-//   localeSubpaths: true
-// };
-
-// const localeSubpathVariations = {
-//   none: {},
-//   foreign: {
-//     fr: "fr"
-//   },
-//   all: {
-//     en: "en",
-//     fr: "fr"
-//   }
-// };
 
 module.exports = new NextI18Next({
   defaultLanguage: "en",
-  // otherLanguages: ["fr", "de", "may"],
   otherLanguages: ["fr"],
   localeSubpaths: {
     fr: "fr",
-    en: "en",
-    // de: "de",
-    // may: "may"
+    en: "en"
   },
-  // serverLanguageDetection = false ---> will disable the cache to save in the cookies for the first load and for the other refereshes
   serverLanguageDetection: false,
   browserLanguageDetection: false,
   detection: {
